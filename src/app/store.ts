5665f257e87f5bcf79a3b60ead1c09507dbe405f
Simon Archer <simon.james.archer@gmail.com>
2023-02-01 21:54:46 +0000

    Ignoring serializable check for redux-persist

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,25 +1,28 @@
-import {
-  AnyAction,
-  combineReducers,
-  configureStore,
-} from "@reduxjs/toolkit";
+import { AnyAction, combineReducers, configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
 import userReducer, { logout } from "../features/user/userSlice";
 import metersReducer from "../features/meters/metersSlice";
-import consumptionReducer from "../features/consumption/consumptionSlice"
-import { persistStore, persistReducer } from 'redux-persist'
-import storage from 'redux-persist/lib/storage'
+import consumptionReducer from "../features/consumption/consumptionSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 const appReducer = combineReducers({
   user: userReducer,
   meters: metersReducer,
-  consumption: consumptionReducer
+  consumption: consumptionReducer,
 });
 
 const rootReducer = (
-  state:
-    | ReturnType<typeof appReducer>
-    | undefined,
+  state: ReturnType<typeof appReducer> | undefined,
   action: AnyAction
 ) => {
   if (action.type === logout.type) {
@@ -29,15 +32,22 @@ const rootReducer = (
 };
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-}
+};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
@@ -45,4 +55,4 @@ export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
